aa2444e7d16ac2f23e3f7da6d9c550d527903f81
Vikas Agarwal <vikasagarwal1750@gmail.com>
2026-04-02 18:39:39 +0530

    fix: serve frontend static files in production and fix CORS for render deployment

--- backend/index.js
+++ backend/index.js
@@ -45,11 +45,13 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production"?
+    "https://chat-app-c5n4.onrender.com" :
+    "http://localhost:5173",
     credentials: true,
   })
 );
@@ -61,7 +63,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
