d78122b92725401fa794b7e8f1083c3dedccaac2
Vikas Agarwal <vikasagarwal1750@gmail.com>
2026-04-02 19:09:04 +0530

    fix: set sameSite=none and secure=true for cookies in production

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -6,8 +6,8 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        secure: process.env.NODE_ENV === "production",
     })
     return token;
 }
